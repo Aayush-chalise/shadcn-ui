48b9dee5ee387609e3093d05c0bf6fd7490e61c9
Aayush Chalise <chaliseaayush2006@gmail.com>
2026-01-05 19:12:43 +0545

    sidebar setup complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import "./App.css";
 import { Button } from "@/components/ui/button";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/app-sidebar";
 
-function App() {
+function App({ children }) {
   return (
     <>
+      <SidebarProvider>
+        <AppSidebar />
+        <main>
+          <SidebarTrigger />
+          {children}
+        </main>
+      </SidebarProvider>
       <div className="flex min-h-svh flex-col items-center justify-center">
         <Button variant="secondary">Click me</Button>
       </div>
